6081a9f777bd4d09eaae3bc60cdfa7be5fcd39c6
bitflame <monazzam@protonmail.com>
2024-04-21 16:28:08 -0400

    made some updates

--- ExcerFourThirty.java
+++ ExcerFourThirty.java
@@ -1,38 +1,46 @@
 
 import edu.princeton.cs.algs4.In;
+
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.Graph;
-import edu.princeton.cs.algs4.Queue;;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;;
 
 public class ExcerFourThirty {
     boolean[] marked;
     Queue<Integer> queue = new Queue<>();
     int[] edgeTo;
+    Stack<Integer> cycle = new Stack<Integer>();
 
     public boolean eulerian(Graph G) {
         // Find the cycle, save it on a stack then check to see if all the vertices have
         // even degrees or not
         marked = new boolean[G.V()];
         edgeTo = new int[G.V()];
+        Arrays.fill(edgeTo, -1);
+        edgeTo[0] = 0;
         for (int s = 0; s < G.V(); s++) {
-            bfs(G, s);
+            dfs(G, s, s, false);
         }
         return false;
     }
 
-    private void bfs(Graph G, int v) {
+    private void dfs(Graph G, int v, int u, boolean eularian) {
         marked[v] = true;
-        queue.enqueue(v);
-        while (!queue.isEmpty()) {
-            v = queue.dequeue();
-            for (int w : G.adj(v)) {
-                if (!marked[w]) {
-                    queue.enqueue(w);
-                    marked[w] = true;
-                    edgeTo[w] = v;
-                } else {
-                    // found a cycle
-                    for (int i = w; i != v; i = edgeTo[i]) {
-                        // push i on to a stack to get the cycle's path
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w, u, eularian);
+            } else if (w != u) {
+                // we have a cycle
+                for (int i = w; i != u; i = edgeTo[i]) {
+                    if (G.degree(i) % 2 == 0) {
+                        eularian = true;
+                    } else {
+                        // if you find one odd degree there is not Eularian cycle; return
+                        eularian = false;
+                        break;
                     }
                 }
             }
